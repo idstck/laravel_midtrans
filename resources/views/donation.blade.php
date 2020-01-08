2a24b531c06af06e5ef91ba39f46bddcb4f0c83e
idstack <idstackdeveloper@gmail.com>
2020-01-08 12:57:43 +0000

    Make Controller and Index Page

---
+++ resources/views/donation.blade.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>IDonation</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+    <style>
+        body {
+            min-height: 75rem;
+        }
+    </style>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">IDonation</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Donation <span class="sr-only">(current)</span></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="jumbotron">
+        <div class="container">
+            <h1 class="display-4">IDonation</h1>
+            <p class="lead">Platform donasi untuk saudara kita yang membutuhkan.</p>
+        </div>
+    </div>
+
+    <div class="container">
+        <form action="#" id="donation_form">
+            <legend>Donation</legend>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="">Nama</label>
+                        <input type="text" name="donor_name" class="form-control" id="donor_name">
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="">E-Mail</label>
+                        <input type="email" name="donor_email" class="form-control" id="donor_email">
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group">
+                        <label for="">Jenis Donasi</label>
+                        <select name="donation_type" class="form-control" id="donation_type">
+                          <option value="medis_kesehatan">Medis & Kesehatan</option>
+                          <option value="kemanusiaan">Kemanusiaan</option>
+                          <option value="bencana_alam">Bencana Alam</option>
+                          <option value="rumah_ibadah">Rumah Ibadah</option>
+                          <option value="beasiswa_pendidikan">Beasiswa & Pendidikan</option>
+                          <option value="sarana_infrastruktur">Sarana & Infrastruktur</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="">Jumlah</label>
+                        <input type="number" name="amount" class="form-control" id="amount">
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="">Catatan (Opsional)</label>
+                        <textarea name="note" cols="30" rows="3" class="form-control" id="note"></textarea>
+                    </div>
+                </div>
+            </div>
+
+            <button class="btn btn-primary" type="submit">Kirim</button>
+        </form>
+    </div>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js">
+    </script>
+</body>
+
+</html>
