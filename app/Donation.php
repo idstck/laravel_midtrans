3303b0a087120997ac27fbc168131414851dea75
idstack <idstackdeveloper@gmail.com>
2020-01-08 12:13:11 +0000

    Make Model and Migration

---
+++ app/Donation.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Donation extends Model
+{
+    protected $guarded = [];
+
+    /**
+     * Set status to Pending
+     *
+     * @return void
+     */
+    public function setStatusPending()
+    {
+        $this->attributes['status'] = 'pending';
+        self::save();
+    }
+
+    /**
+     * Set status to Success
+     *
+     * @return void
+     */
+    public function setStatusSuccess()
+    {
+        $this->attributes['status'] = 'success';
+        self::save();
+    }
+
+    /**
+     * Set status to Failed
+     *
+     * @return void
+     */
+    public function setStatusFailed()
+    {
+        $this->attributes['status'] = 'failed';
+        self::save();
+    }
+
+    /**
+     * Set status to Expired
+     *
+     * @return void
+     */
+    public function setStatusExpired()
+    {
+        $this->attributes['status'] = 'expired';
+        self::save();
+    }
+}
---
+++ database/migrations/2020_01_08_115012_create_donations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDonationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('donations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('donor_name')->nullable();
+            $table->string('donor_email')->nullable();
+            $table->string('donation_type')->nullable();
+            $table->decimal('amount', 20, 2)->default(0);
+            $table->string('note')->nullable();
+            $table->string('status')->default('pending');
+            $table->string('snap_token')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('donations');
+    }
+}
